2c53aebbab5b07e98d7f8cc63aa3142e6a3415ff
Gr3gg0r <48479663+Gr3gg0r@users.noreply.github.com>
2020-07-02 14:06:46 +0800

    sad
    asda

---
+++ Vilrux.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.4.18;
+
+contract VilRux {
+    
+    string  fname ;
+    uint  age ;
+    
+    function setInstructor(string _fname, uint _age) public {
+        fname = _fname;
+        age =_age;
+    }
+    
+    function getInstructor() public constant returns (string ,uint) {
+        return(fname,age);
+    }
+    
+    
+}
